ef230e296ead951a641290c7186323953eb316a6
Emanherawy <eman.herawy@gmail.com>
2023-11-18 17:43:55 +0300

    update registration

--- smart_contracts/contracts/Registration.sol
+++ smart_contracts/contracts/Registration.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// the contract should manage user registration , for creator and contributors
 
@@ -11,8 +12,17 @@ contract Registration is Pausable, Ownable {
     mapping (string=>address) public  githubToAddress;
     mapping (address=>string) public addressToGithub;
     mapping (address=>bool) public isOpenSourceProjectCreator;
-
-    constructor() {}
+    IERC20 daoToken;
+    uint256 public blanceToJoin;
+    constructor(address tokenId , uint256 _blanceToJoin)  {
+        daoToken = IERC20(tokenId);
+        require(_blanceToJoin>10,"balance must be greater than 10");
+        blanceToJoin = _blanceToJoin;
+    }
+    function updateBlanceToJoin(uint256 _blanceToJoin) public onlyOwner{
+        require(_blanceToJoin>10,"balance must be greater than 10");
+        blanceToJoin = _blanceToJoin;
+    }
 
         // register as contributor
 
@@ -25,9 +35,12 @@ contract Registration is Pausable, Ownable {
 
 
     // join as open-source-project-creator
+    // later we need to handle case of creator manipulating the contract by selling the token after joining 
 
     function joinAsOpenSourceProjectCreator() public  whenNotPaused{
         require(! isOpenSourceProjectCreator[msg.sender], "already registered as open source project creator");
+      /// make sure creator is qualified to join
+        require(daoToken.balanceOf(msg.sender) >= blanceToJoin, "not enough balance to join");
         isOpenSourceProjectCreator[msg.sender] = true;
         emit CreatorRegistered(msg.sender);
     }
