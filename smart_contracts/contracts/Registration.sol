aaf44bb059d92157626700ecfe0d59c3ea6e2590
Emanherawy <eman.herawy@gmail.com>
2023-11-18 17:32:20 +0300

    add initial registration flow

--- smart_contracts/contracts/Registration.sol
+++ smart_contracts/contracts/Registration.sol
@@ -7,7 +7,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// the contract should manage user registration , for creator and contributors
 
 contract Registration is Pausable, Ownable {
+
+    mapping (string=>address) public  githubToAddress;
+    mapping (address=>string) public addressToGithub;
+    mapping (address=>bool) public isOpenSourceProjectCreator;
+
     constructor() {}
+
+        // register as contributor
+
+    function joinAsContributor(string memory githubUsername) public  whenNotPaused{
+        require(githubToAddress[githubUsername] == address(0), "github username already registered");
+        githubToAddress[githubUsername] = msg.sender;
+        addressToGithub[msg.sender] = githubUsername;
+        emit ContributorRegistered(msg.sender, githubUsername);
+    }
+
+
+    // join as open-source-project-creator
+
+    function joinAsOpenSourceProjectCreator() public  whenNotPaused{
+        require(! isOpenSourceProjectCreator[msg.sender], "already registered as open source project creator");
+        isOpenSourceProjectCreator[msg.sender] = true;
+        emit CreatorRegistered(msg.sender);
+    }
+
+
+    // leave 
+
 
     function pause() public onlyOwner {
         _pause();
@@ -16,5 +43,10 @@ contract Registration is Pausable, Ownable {
     function unpause() public onlyOwner {
         _unpause();
     }
+
+    event CreatorRegistered(address indexed creatorAddress);
+    event ContributorRegistered(address indexed contributorAddress, string indexed githubUsername);
+     
+
 }
 
