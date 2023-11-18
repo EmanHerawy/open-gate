ab5ce628b30c5eb6bb05a80c2fcca501deee13d4
ismail9k <dev@ismail9k.com>
2023-11-18 05:17:22 +0300

    feat: add basic pages

---
+++ app/app/tasks/page.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+export default function HomePage() {
+  return (
+    <Box>
+      <Box sx={{ mb: 5 }}>
+        <Typography variant="h2" component="h2">
+          Tasks
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
