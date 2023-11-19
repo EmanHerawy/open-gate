7792b7ff4af0eace4d86bc8ce334714167d431ff
ismail9k <dev@ismail9k.com>
2023-11-19 07:57:08 +0300

    fix: chainlink function

--- smart_contracts/deploy/3.pool.deploy.ts
+++ smart_contracts/deploy/3.pool.deploy.ts
@@ -13,7 +13,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const bugbounty = 2;
       const _checkLogic=`const repo = args[0];
 const prNum = args[1];
-const token = secrets.token; 
+const token = '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'; 
 
 if (!token) {
   throw Error("Missing secret: github token");
@@ -43,7 +43,7 @@ console.log("API response data:", JSON.stringify(data, null, 2));
 const { merged, user, created_at } = data;
 
 if (!merged) {
-  throw new Error('Pull request #' + prNumber 'is not merged or could be closed');
+  throw new Error('Pull request #' + prNumber + 'is not merged or could be closed');
 }
 
 return Functions.encodeString(JSON.stringify({
